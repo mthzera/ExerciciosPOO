312e7380f955117977110da03bcc10d9b8328f83
MATHEUS MISUMI CIMURRO <matheus.mcimurro@senacsp.edu.br>
2017-05-17 19:29:28 -0300

    Exercicio de inverter a ordem digitada

---
+++ busca1_solution/busca1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace busca1
+{
+
+    class aleatorio
+    {
+        int[] numeros = new int[99];
+        Random rand = new Random();
+        int x = 0;
+        public void listaNumeros(int escolha)
+        {
+            for (int i = 0; i < 99; i++)
+            {
+                numeros[i] = rand.Next(10) + 1;
+
+            }
+        }
+        public void verifica(int escolha)
+        {
+            for (int i = 0; i < 99; i++)
+            {
+                if (numeros[i] == escolha) {
+                    x = numeros[i];
+                    i = 99;
+                }
+
+            }
+            if (x > 0)
+            {
+                Console.WriteLine(numeros[x]);
+            }
+            else
+            {
+                Console.WriteLine("Não foi encontrado o numero");
+            }
+
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Numero:");
+                int escolha = int.Parse(Console.ReadLine());
+                aleatorio e = new aleatorio();
+                e.listaNumeros(escolha);
+                e.verifica(escolha);
+
+            }
+
+
+
+        }
+    }
+}
---
+++ busca1_solution/inverte/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inverte
+{
+
+    class palavras
+    {
+        public palavras()
+        {
+            string[] vetorPalavras = new string[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Digite uma palavra");
+                vetorPalavras[i] = Console.ReadLine();
+            }
+
+            for(int i = 9; i >= 0; i--)
+            {
+                Console.WriteLine(vetorPalavras[i]);
+            }
+            
+            
+
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            palavras p = new palavras();
+        }
+    }
+}
