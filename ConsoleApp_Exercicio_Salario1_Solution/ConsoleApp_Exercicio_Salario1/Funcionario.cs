01670983572f3ec4bf5b2c1e4b758cab6e59fb61
MATHEUS MISUMI CIMURRO <matheus.mcimurro@senacsp.edu.br>
2017-05-17 19:30:40 -0300

    Adicionando exercicio salario

---
+++ ConsoleApp_Exercicio_Salario1_Solution/ConsoleApp_Exercicio_Salario1/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Exercicio_Salario1
+{
+    class Funcionario
+    {
+        public string nome;
+        public decimal valorHora;
+        public double hora;
+
+        public Funcionario(string nome, decimal valorHora, double hora)
+        {
+            this.nome = nome;
+            this.valorHora = valorHora;
+            this.hora = hora;
+        }
+
+
+        public decimal getSalario()
+        {
+            return Convert.ToDecimal(hora) * valorHora; 
+        }
+    }
+}
---
+++ ConsoleApp_Exercicio_Salario1_Solution/ConsoleApp_Exercicio_Salario1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Exercicio_Salario1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // ENTRADAS
+            Console.WriteLine("Digite o nome do funcionario");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o valor da Hora: ");
+            decimal valorHora = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o total da Horas: ");
+            double hora = double.Parse(Console.ReadLine());
+
+            // PROCESSAMENTO
+
+            //Criado o objeto 
+            Funcionario f = new Funcionario(nome, valorHora, hora);
+            decimal salario = f.getSalario();
+
+
+            //SAIDA
+
+
+            Console.WriteLine("“O funcionário {0} trabalhou {1} horas por R$ {2} e deve receber R${3}.", nome, hora, valorHora, salario );
+            
+
+
+
+
+
+        }
+    }
+}
